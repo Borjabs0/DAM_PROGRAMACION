4986f3d748e2d99d30c297adb5268aaee68a2148
Borjabs0 <146438165+Borjabs0@users.noreply.github.com>
2024-02-16 19:12:47 +0100

    Commit clases: Atencion.java ConsoleMenu.java LibIO.java

---
+++ src/main/java/com/borjabolufer/Lib/LibIO.java
@@ -0,0 +1,54 @@
+package com.borjabolufer.Lib;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class LibIO {
+    private  static  final Scanner scanner = new Scanner(System.in);
+    public static  String solicitarString(String mensaje, int longitudMinima, int longitudMaxima){
+        String result;
+        boolean valido;
+        do {
+            System.out.println(mensaje);
+            result = scanner.nextLine();
+            valido = result.length() >= longitudMinima && result.length() <= longitudMaxima;
+            if (!valido)
+                System.err.printf("La longitud debe estar entre %d y %d", longitudMinima, longitudMaxima);
+
+        }while(!valido);
+        return result;
+    }
+    public  static Date solicitarFechaDate(String mensaje, SimpleDateFormat formato){
+        Date fecha = null;
+        boolean valido;
+        do {
+            System.out.println(mensaje);
+            String fechaStr = scanner.nextLine();
+            try {
+                fecha = formato.parse(fechaStr);
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+            valido = fecha != null;
+            if (!valido){
+                System.out.println("El formato de la fecha no es válido");
+            }
+        }while (!valido);
+        return fecha;
+    }
+    public static  double solicitarDouble(String mensaje, double valorMinimo, double valorMaximo){
+        double result;
+        boolean valido;
+        do {
+            System.out.println(mensaje);
+            result = Double.parseDouble(scanner.nextLine());
+            valido = result >= valorMinimo && result <= valorMaximo;
+            if (!valido)
+                System.err.printf("La longitud debe estar entre %d y %d", valorMinimo, valorMaximo);
+
+        }while(!valido);
+        return result;
+    }
+}
---
+++ src/main/java/com/borjabolufer/segundotrimestre/tema07/Atencion.java
@@ -0,0 +1,147 @@
+package com.borjabolufer.segundotrimestre.tema07;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class Atencion {
+    private static final int POSICION_TEMPERATURA = 0;
+    private static final int POSICION_PPM = 1;
+    private static final int POSICION_TENSION_SISTOLICA = 2;
+    private static final int POSICION_TENSION_DIASTOLICA = 3;
+    private final Date fechaEntrada;
+    private final String sintomatologia;
+    private double[] preRev;
+    private final Paciente paciente;
+    private String motivoAlta;
+    private Date fechaAlta;
+
+    public Atencion(Paciente paciente, Date fechaEntrada, String sintomatologia) {
+        this.paciente = paciente;
+        this.fechaEntrada = fechaEntrada;
+        this.sintomatologia = sintomatologia;
+        preRev = null;
+    }
+
+    public Atencion(Atencion atencion){
+        this.fechaEntrada = new Date(atencion.fechaEntrada.getTime());
+        this.sintomatologia =  new String(atencion.sintomatologia);
+        this.paciente = new Paciente(atencion.paciente);
+
+        if (atencion.preRev != null) {
+            this.preRev = new double[atencion.preRev.length];
+            for (int i = 0; i < preRev.length; i++) {
+                preRev[i] = atencion.preRev[i];
+
+            }
+        }
+        if (atencion.fechaAlta != null) {
+            this.fechaAlta = new Date(atencion.fechaAlta.getTime());
+        }
+        if (atencion.motivoAlta != null) {
+            this.motivoAlta = new String(atencion.motivoAlta);
+        }
+
+    }
+
+    public boolean isAtendido() {
+        return preRev != null;
+    }
+
+    public void altaPaciente(Date fechaAlta, String motivoAlta) {
+        this.fechaAlta = fechaAlta;
+        this.motivoAlta = motivoAlta;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public Date getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public String getSintomatologias() {
+        return sintomatologia;
+    }
+
+    public void setConstantesVitales(double temperatura, double ppm, double tensionSistolica, double tensionDiastolica) {
+        preRev = new double[4];
+        setTemperatura(temperatura);
+        setPPM(ppm);
+        setTensionSistolica(tensionSistolica);
+        setTensionDiastolica(tensionDiastolica);
+
+    }
+
+    public double getTemperatura() {
+        if (preRev == null)
+            return -1;
+        return preRev[POSICION_TEMPERATURA];
+    }
+
+    private void setTemperatura(double temperatura) {
+        preRev[POSICION_TEMPERATURA] = temperatura;
+    }
+
+    public double getPPM() {
+        if (preRev == null)
+            return -1;
+        return preRev[POSICION_PPM];
+    }
+
+    private void setPPM(double ppm) {
+        preRev[POSICION_PPM] = ppm;
+    }
+
+    public double getTensionSistolica() {
+        if (preRev == null)
+            return -1;
+        return preRev[POSICION_TENSION_SISTOLICA];
+    }
+
+    private void setTensionSistolica(double tensionSistolica) {
+        preRev[POSICION_TENSION_SISTOLICA] = tensionSistolica;
+    }
+
+    public double getTensionDiastolica() {
+        if (preRev == null)
+            return -1;
+        return preRev[POSICION_TENSION_DIASTOLICA];
+    }
+
+    private void setTensionDiastolica(double tensionDiastolica) {
+        preRev[POSICION_TENSION_DIASTOLICA] = tensionDiastolica;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Atencion atencion = (Atencion) o;
+
+        if (!fechaEntrada.equals(atencion.fechaEntrada)) return false;
+        return paciente.equals(atencion.paciente);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = fechaEntrada.hashCode();
+        result = 31 * result + paciente.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Atencion(" +
+                "paciente= " + paciente.getSip() + "" + paciente.getNombre() +
+                "fechaEntrada=" + fechaEntrada +
+                ", sintomatologia='" + sintomatologia + '\'' +
+                ", preRev=" + Arrays.toString(preRev) +
+                ", paciente=" + paciente +
+                ", motivoAlta='" + motivoAlta + '\'' +
+                ", fechaAlta=" + fechaAlta +
+                ")\n";
+    }
+}
