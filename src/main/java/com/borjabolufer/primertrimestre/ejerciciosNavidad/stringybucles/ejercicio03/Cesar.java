ed2e2e9bf7c374c2ee4af6e16710c10479885245
Borjabs0 <146438165+Borjabs0@users.noreply.github.com>
2024-03-21 13:12:17 +0100

    Commit Ejercicio encriptacion con cifrado cesar

---
+++ src/main/java/com/borjabolufer/primertrimestre/ejerciciosNavidad/stringybucles/ejercicio03/Cesar.java
@@ -0,0 +1,35 @@
+package com.borjabolufer.primertrimestre.ejerciciosNavidad.stringybucles.ejercicio03;
+
+public class Cesar {
+    private final String contrasenya;
+    private final int numero;
+    private final String abecedario = "abcdefghijklmnñopqrstuvwxyz";
+    private final String abecedarioMayu = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+
+    public Cesar(String contrasenya, int numero) {
+        this.contrasenya = contrasenya;
+        this.numero = numero;
+    }
+
+
+    public String cifrar(){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < contrasenya.length(); i++){
+            char posicionPass = contrasenya.charAt(i);
+            if (Character.isLowerCase(posicionPass)) {
+                int indiceOriginal = abecedario.indexOf(posicionPass);
+                int nuevoIndice = (indiceOriginal + numero) % abecedario.length();
+                char ecriptado = abecedario.charAt(nuevoIndice);
+                sb.append(ecriptado);
+            }
+            else{
+                int indiceOriginal = abecedarioMayu.indexOf(posicionPass);
+                int nuevoIndice = (indiceOriginal + numero) % abecedarioMayu.length();
+                char ecriptado = abecedarioMayu.charAt(nuevoIndice);
+                sb.append(ecriptado);
+            }
+        }
+    return sb.toString();
+    }
+
+}
---
+++ src/main/java/com/borjabolufer/primertrimestre/ejerciciosNavidad/stringybucles/ejercicio03/Ejercicio03.java
@@ -0,0 +1,8 @@
+package com.borjabolufer.primertrimestre.ejerciciosNavidad.stringybucles.ejercicio03;
+
+public class Ejercicio03 {
+    public static void main(String[] args) {
+        Cesar cesar =new Cesar("Claudia", 3);
+        System.out.println(cesar.cifrar());
+    }
+}
