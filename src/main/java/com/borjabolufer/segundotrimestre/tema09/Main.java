30755479607980d1695bcad5d489992bb3dbb5ca
Borjabs0 <146438165+Borjabs0@users.noreply.github.com>
2024-04-12 12:22:07 +0200

    Commit de ejercicios 3 del  tema excepciones

--- src/main/java/com/borjabolufer/segundotrimestre/tema09/Main.java
+++ src/main/java/com/borjabolufer/segundotrimestre/tema09/Main.java
@@ -2,11 +2,13 @@ package com.borjabolufer.segundotrimestre.tema09;
 
 import com.borjabolufer.segundotrimestre.tema09.ejercicio01.Ejercicio01;
 import com.borjabolufer.segundotrimestre.tema09.ejercicio02.Ejercicio02;
+import com.borjabolufer.segundotrimestre.tema09.ejercicio03.Ejercicio03;
 
 public class Main {
     public static void main(String[] args) {
         // Ejercicio01 ejercicio01 = new Ejercicio01();
         // Ejercicio02 ejercicio02 = new Ejercicio02();
+        Ejercicio03 ejercicio03 = new Ejercicio03();
 
 
     }
