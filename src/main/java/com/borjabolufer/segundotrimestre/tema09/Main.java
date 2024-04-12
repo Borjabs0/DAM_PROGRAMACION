35158902f80e7dc1f23d0274418d9609ad1ab13a
Borjabs0 <146438165+Borjabs0@users.noreply.github.com>
2024-04-12 14:01:34 +0200

    Commit de ejercicios 4 del  tema excepciones

--- src/main/java/com/borjabolufer/segundotrimestre/tema09/Main.java
+++ src/main/java/com/borjabolufer/segundotrimestre/tema09/Main.java
@@ -3,13 +3,17 @@ package com.borjabolufer.segundotrimestre.tema09;
 import com.borjabolufer.segundotrimestre.tema09.ejercicio01.Ejercicio01;
 import com.borjabolufer.segundotrimestre.tema09.ejercicio02.Ejercicio02;
 import com.borjabolufer.segundotrimestre.tema09.ejercicio03.Ejercicio03;
+import com.borjabolufer.segundotrimestre.tema09.ejercicio04.Ejercicio04;
 
 public class Main {
     public static void main(String[] args) {
-        // Ejercicio01 ejercicio01 = new Ejercicio01();
-        // Ejercicio02 ejercicio02 = new Ejercicio02();
-        Ejercicio03 ejercicio03 = new Ejercicio03();
-
+//        Ejercicio01 ejercicio01 = new Ejercicio01();
+//        Ejercicio02 ejercicio02 = new Ejercicio02();
+//        Ejercicio03 ejercicio03 = new Ejercicio03();
+//        Ejercicio04 ejercicio04 = new Ejercicio04();
+//        int[] array = new int[5];
+//        int[] array2;
+//        ejercicio04.solicitarNumeros(array2);
 
     }
 
---
+++ src/main/java/com/borjabolufer/segundotrimestre/tema09/ejercicio04/Ejercicio04.java
@@ -0,0 +1,39 @@
+package com.borjabolufer.segundotrimestre.tema09.ejercicio04;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ejercicio04 {
+    private Scanner scanner;
+
+    public void solicitarNumeros(int[] array) {
+        scanner = new Scanner(System.in);
+        if (array == null) {
+            System.out.println("El array no está creado");
+            return;
+        }
+        for (int i = 0; i < array.length; i++) {
+            try {
+                System.out.println("Inserte un numero: ");
+                array[i] = Integer.parseInt(scanner.nextLine());
+                System.out.println("Numero insertado correctamente");
+            } catch (NumberFormatException e) {
+                System.out.println("Se ha insertado una letra. Vuele a intentarlo");
+                i--; // Volvemos a intentar ingresar el numero en la misma posición
+            } catch (ArrayIndexOutOfBoundsException b) {
+                System.out.println("As sobrepasado la canatidad del array");
+                break;
+            }
+        }
+        //Mostrar el contenido del array
+        System.out.println();
+        System.out.println("Array: ");
+        int cont = 0;
+        for (int i : array) {
+            System.out.println("Posicion " + cont++ + ": " + i + " ");
+        }
+    }
+}
+
+
